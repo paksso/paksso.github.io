e159233b189f2866cf9fca3194672eba03c69e2f
paksso <sophie2012001@gmail.com>
2026-07-24 14:02:26 +0900

    Delete campmalchin_html directory

--- campmalchin_html/js/menu.js
+++
@@ -1,9 +0,0 @@
-function openMenu() {
-    document.getElementById("sideMenu").style.left = "0";
-    document.getElementById("overlay").style.display = "block";
-}
-
-function closeMenu() {
-    document.getElementById("sideMenu").style.left = "-250px";
-    document.getElementById("overlay").style.display = "none";
-}
